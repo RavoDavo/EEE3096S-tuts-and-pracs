a521587d25de07adf243eafe434bbfa13fe35b9d
Theodore Psillos <PSLTHE001@myuct.ac.za>
2021-08-14 19:59:49 +0100

    First commit of main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main(){
+        int a, b, sum;
+
+        printf("Enter a value of a: ");
+        scanf("%d", &a);
+
+        printf("Enter a value of b: ");
+        scanf("%d", &b);
+ 
+        sum = a + b;
+
+        printf("The sum of a and b is %d \n.",sum);
+}
