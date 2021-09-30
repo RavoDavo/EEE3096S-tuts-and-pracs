8507008147c157355b0ea2bf85fddb77f81deb45
Theodore Psillos <PSLTHE001@myuct.ac.za>
2021-09-30 10:52:19 +0100

    updated workpackage4

--- EEE3096S-2021/WorkPackage4/py3.py
+++ EEE3096S-2021/WorkPackage4/py3.py
@@ -1,8 +1,11 @@
 import busio
 import digitalio
 import board
+from time import sleep
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+
+#board.D16 for the GPIO16
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -14,7 +17,13 @@ cs = digitalio.DigitalInOut(board.D5)
 mcp = MCP.MCP3008(spi, cs)
 
 # create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+Temp = AnalogIn(mcp, MCP.P1)
+LDR = AnalogIn(mcp, MCP.P2)
 
-print(’Raw ADC Value: ’, chan.value)
-print(’ADC Voltage: ’ + str(chan.voltage) + ’V’)
+print("Runtime        Temp Reading    Temp          Light Reading      ")
+i = 0
+while (True):
+	print(str(i)+'s            '+ str(Temp.value)+'  	       '+str(Temp.value)+" C       "+str(LDR.voltage))
+	#print('ADC Voltage: ' + str(chan.voltage) + 'V')
+	i = i+10
+	sleep(1.5)
