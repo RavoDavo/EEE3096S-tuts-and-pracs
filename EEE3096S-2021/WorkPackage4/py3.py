6d1b8049dec0fc9159e5a62d64c73c91e694fd53
RavoDavo <dcsdav001@myuct.ac.za>
2021-10-05 10:20:16 +0100

    Changed p3.py

--- EEE3096S-2021/WorkPackage4/py3.py
+++ EEE3096S-2021/WorkPackage4/py3.py
@@ -1,14 +1,12 @@
 import busio
 import digitalio
 import board
-from time import sleep
+import time 
+
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import threading
-import datetime
 #board.D16 for the GPIO16
-
-btn_sample_rate 
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -18,47 +16,64 @@ cs = digitalio.DigitalInOut(board.D5)
 
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
-
 # create an analog input channel on pin 0
 Temp = AnalogIn(mcp, MCP.P1)
 LDR = AnalogIn(mcp, MCP.P2)
+BTN=23
+level = 0
+modes = [10, 5, 1]
+period = modes[level]
 
 V0 = 0.5
 TC = 0.01
+def setup():
+	GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.add_event_detect(btn_pin, GPIO.RISING, callback=increment_index, bouncetime=250)
+	pass
 
-def config():
-	"""
-	Setup the button and callbacks 
-	"""
-	global btn_sample_rate 
-	btn_sample_rate = digitalio.DigitalInOut(board.D16) # Sets the GPIO numbering to board numbers
-	switch.switch_to_input(pull=digitalio.Pull.UP) # If pull up doesnt work use Pull Down
-	# Need to implement manual debouncing 
 
 def printADC():
 	"""
 	This function formats the prinnting of the ADC values
 	"""
 	TA = (Temp.voltage-V0)/TC
-	print(datetime.datetime.now()+'s\t\t'+ str(Temp.value)+'\t\t\t'+str(round(TA,2))+" C\t\t"+str(LDR.value))
+	
+	
+
+	print(str(int(round(run_time, 0)))+'s\t\t'+ str(Temp.value)+'\t\t\t'+str(round(TA,2))+" C\t\t"+str(LDR.value))
+	
+
 	
 
 def print_sensor_thread():
 	"""
 	This function prints the ADC sensor values to the screen every ten seconds
 	"""
-	thread = threading.Timer(10.0, print_sensor_thread)
+	period = modes[level]
+	thread = threading.Timer(period, print_sensor_thread)
 	thread.daemon = True  # Daemon threads exit when the program does
 	thread.start()
+	global runtime
+	runtime=time.time()-tic
 	printADC()
+	pass
+
+def increment_index(self):
+	global level
+	if (level > 1):
+		level = 0
+	else:
+		level += 1
+	
+	pass
 
 if __name__ == "__main__":
-	config()
+	
 	print("Runtime\t\tTemp Reading\t\tTemp\t\tLight Reading")
+	tic=time.time()
 	print_sensor_thread() # call it once to start the thread
 
 	# Tell our program to run indefinitely
 	while True:
-		# Check button press
 		pass
 
