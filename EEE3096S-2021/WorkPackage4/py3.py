42cea523b94f8ced5228dba8a22125d379b8fc1e
RavoDavo <dcsdav001@myuct.ac.za>
2021-10-05 15:05:57 +0100

    adjusted p3.py

--- EEE3096S-2021/WorkPackage4/py3.py
+++ EEE3096S-2021/WorkPackage4/py3.py
@@ -2,7 +2,7 @@ import busio
 import digitalio
 import board
 import time 
-
+import RPI.GPIO as GPIO
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import threading
@@ -19,7 +19,7 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 Temp = AnalogIn(mcp, MCP.P1)
 LDR = AnalogIn(mcp, MCP.P2)
-BTN=23
+BTN=23 #GPIO pin
 level = 0
 modes = [10, 5, 1]
 period = modes[level]
@@ -27,8 +27,8 @@ period = modes[level]
 V0 = 0.5
 TC = 0.01
 def setup():
-	GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(btn_pin, GPIO.RISING, callback=increment_index, bouncetime=250)
+	GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.add_event_detect(BTN, GPIO.FALLING, callback=increment_index, bouncetime=250)
 	pass
 
 
@@ -40,7 +40,7 @@ def printADC():
 	
 	
 
-	print(str(int(round(run_time, 0)))+'s\t\t'+ str(Temp.value)+'\t\t\t'+str(round(TA,2))+" C\t\t"+str(LDR.value))
+	print(str(int(round(runtime, 0)))+'s\t\t'+ str(Temp.value)+'\t\t\t'+str(round(TA,2))+" C\t\t"+str(LDR.value))
 	
 
 	
@@ -60,7 +60,7 @@ def print_sensor_thread():
 
 def increment_index(self):
 	global level
-	if (level > 1):
+	if (level == 2):
 		level = 0
 	else:
 		level += 1
@@ -68,12 +68,17 @@ def increment_index(self):
 	pass
 
 if __name__ == "__main__":
-	
-	print("Runtime\t\tTemp Reading\t\tTemp\t\tLight Reading")
-	tic=time.time()
-	print_sensor_thread() # call it once to start the thread
+	try:
+		setup()
+		print("Runtime\t\tTemp Reading\t\tTemp\t\tLight Reading")
+		tic=time.time()
+		print_sensor_thread() # call it once to start the thread
 
-	# Tell our program to run indefinitely
-	while True:
-		pass
+		# Tell our program to run indefinitely
+		while True:
+			pass
 
+	except Exception as e:
+		print(e)
+	finally:
+		GPIO.cleanup()
